649cf7dd651fb859f9faeef1e5aa1d6721664ce6
Maria Mercado <maria.mercado@salesforce.com>
2024-08-08 11:30:36 -0700

    black

--- listeners/listener_utils/parse_conversation.py
+++ listeners/listener_utils/parse_conversation.py
@@ -2,6 +2,7 @@ from logging import Logger
 from typing import Optional, List
 from slack_sdk.web.slack_response import SlackResponse
 
+
 # Parses a conversation history, excluding messages from the bot,
 # and formats it as a string with user IDs and their messages.
 # Used in `app_mentioned_callback`, `dm_sent_callback`,
