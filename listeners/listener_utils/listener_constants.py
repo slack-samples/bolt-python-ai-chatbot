0cc9e9d7ebd656bba74ab9c54fb3d895ccea0665
JIMBA Koji <dj.jimba@gmail.com>
2025-02-19 02:18:13 +0900

    Fix prompt for channel conversation summarization (#48)
    Co-authored-by: Eden Zimbelman <zim@o526.net>

--- listeners/listener_utils/listener_constants.py
+++ listeners/listener_utils/listener_constants.py
@@ -7,8 +7,8 @@ Hi there! You didn't provide a message with your mention.
     Mention me again in this thread so that I can help you out!
 """
 SUMMARIZE_CHANNEL_WORKFLOW = """
-User has just joined this slack channel.
-Create a quick summary of the conversaton in this channel to cath up the user.
-Don't use user names in your response.
+A user has just joined this Slack channel.
+Please create a quick summary of the conversation in this channel to help them catch up.
+Don't use user IDs or names in your response.
 """
 DEFAULT_LOADING_TEXT = "Thinking..."
